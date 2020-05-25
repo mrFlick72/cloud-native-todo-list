1be246d0291ee4c82efb1778402898a4bd2180b9
mrflick72 <valerio.vaudi@gmail.com>
2020-05-25 23:34:02 +0200

    openid connect login

--- todo-list-website/src/main/kotlin/it/valeriovaudi/todolistwebsite/TodoListWebSiteApplication.kt
+++ todo-list-website/src/main/kotlin/it/valeriovaudi/todolistwebsite/TodoListWebSiteApplication.kt
@@ -2,6 +2,12 @@ package it.valeriovaudi.todolistwebsite
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.client.RestTemplate
+import java.util.Map
 
 
 @SpringBootApplication
@@ -11,4 +17,18 @@ class TodoListWebSiteApplication {
 
 fun main(args: Array<String>) {
     runApplication<TodoListWebSiteApplication>(*args)
+}
+
+
+@EnableWebSecurity
+class OAuth2SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+                .authorizeRequests().mvcMatchers("/actuator/**").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().oauth2Login().defaultSuccessUrl("/index.html")
+    }
+
 }
